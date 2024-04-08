11c4665c0c04eae4c2e8ec7b14ea3eeb3a1c275d
Durga Nagarjuna <durganagarjunap@gmail.com>
2024-04-08 18:40:21 +0000

    Commit 3: Added new print meassage in the code

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 //This program will print helloworld
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Git VCS!");  
     }
 }
